b2d2f926a9d1c5691938fc63c72d12941bcc9db2
jrgatsb <jrg@statsbiblioteket.dk>
2013-04-05 15:41:12 +0200

    Model-classes for mapping uuids to content

--- content-resolver-model/src/main/java/dk/statsbiblioteket/medieplatform/contentresolver/model/Content.java
+++ content-resolver-model/src/main/java/dk/statsbiblioteket/medieplatform/contentresolver/model/Content.java
@@ -21,15 +21,12 @@ package dk.statsbiblioteket.medieplatform.contentresolver.model;
  */
 
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
-import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * List of available content.
  */
-@XmlRootElement(namespace = "http://medieplatform.statsbiblioteket.dk/contentresolver/")
 public class Content {
     /** Available contents. */
     private List<Resource> resources = new ArrayList<Resource>();
