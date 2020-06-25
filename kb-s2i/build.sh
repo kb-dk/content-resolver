64d23668e1c2ff77e1b8f0276784b69265857f6e
Kim Christensen <ktc@kb.dk>
2020-06-25 11:48:39 +0200

    Fix path to actual build artifact

--- kb-s2i/build.sh
+++ kb-s2i/build.sh
@@ -2,7 +2,7 @@
 
 cd /tmp/src
 
-cp -rp -- /tmp/src/target/content-resolver-service/content-resolver-service-*.war "$TOMCAT_APPS/content-resolver-service.war"
+cp -rp -- /tmp/src/content-resolver-service/target/content-resolver-service-*.war "$TOMCAT_APPS/content-resolver-service.war"
 cp -- /tmp/src/conf/ocp/content-resolver.xml "$TOMCAT_APPS/content-resolver.xml"
 
 export WAR_FILE=$(readlink -f "$TOMCAT_APPS/content-resolver-service.war")
